66efb92bb95dd74381091bc76c6734497723cf51
imanmaliki11 <45257900+imanmaliki11@users.noreply.github.com>
2019-05-17 14:16:10 +0700

    Update Mahasiswa.java

--- src/presensi/Mahasiswa.java
+++ src/presensi/Mahasiswa.java
@@ -3,9 +3,11 @@ package presensi;
 public class Mahasiswa extends Person{
     public String Angkatan;
     
-    public void Mahasiswa(String nama, String NIM, String angkatan){
+    public Mahasiswa(String nama, String NIM, String angkatan){
         this.Angkatan = angkatan;
         super.Person(nama, NIM);
     }
+    
+    
             
 }
